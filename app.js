fb327ab3d9731ebc52fc8eb447766d4b874f5837
Euang1812 <bosswjdwo@gmail.com>
2022-03-11 01:20:20 +0900

    Study up to 4-7!

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-const h1 = document.querySelector("h1");
+const $loginForm = document.querySelector("#loginForm");
+const $loginInput = document.querySelector("#loginForm input");
+const $greeting = document.querySelector("#greeting");
 
-function onClickTitle() {
-    const clickClass = "clicked";
 
-    h1.classList.toggle(clickClass);
+const HIDDEN_CLASSNAME = 'hidden'
+const USERNAME_KEY = 'username'
 
-    // toggle로 대체가능
-    // if (h1.classList.contains(clickClass)) {
-    //     h1.classList.remove(clickClass);
-    // } else {
-    //     h1.classList.add(clickClass);
-    // }
+function onLoginSubmit(event) {
+  event.preventDefault();
+  $loginForm.classList.add(HIDDEN_CLASSNAME);
+  const userName = $loginInput.value;
+  localStorage.setItem(USERNAME_KEY, userName);
+  paintGreeting(userName,"hi~");
 }
 
-h1.addEventListener("click", onClickTitle);
+function handelLinkClick(event) {
+  alert('click!');
+  event.preventDefault();
+
+}
+
+function paintGreeting(username,hi) {
+  $greeting.textContent = `${hi} ${username}`;
+  $greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+$loginForm.addEventListener("submit", onLoginSubmit);
+
+const saveUserName = localStorage.getItem(USERNAME_KEY);
+
+if (saveUserName === null) {
+  $loginForm.classList.remove(HIDDEN_CLASSNAME);
+  $loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+  paintGreeting(saveUserName,"oh~");
+}
